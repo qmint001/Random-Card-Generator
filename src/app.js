465776b2335651f2af40ae2a6d47e2128dd5ebd3
qmint001 <queenette.minter001@mymdc.net>
2023-03-09 05:43:21 +0000

    Commit 1

--- src/app.js
+++ src/app.js
@@ -2,10 +2,58 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
 
+function changeCard() {
+  const suites = ["♦", "♥", "♠", "♣"];
+  const cards = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "K",
+    "Q",
+    "J",
+    "A"
+  ];
+
+  //center.getElementsByClassName.color = "red";
+  const randomCardIndex = Math.floor(Math.random() * cards.length);
+  const randomCardValue = cards[randomCardIndex];
+
+  const center = document.querySelector("#number");
+  center.innerText = randomCardValue;
+
+  const randomSuiteIndex = Math.floor(Math.random() * suites.length);
+  const randomSuiteValue = suites[randomSuiteIndex];
+
+  const topSuite = document.querySelector("#topsuite");
+  const bottomSuite = document.querySelector("#bottomsuite");
+
+  topSuite.innerText = randomSuiteValue;
+  bottomSuite.innerText = randomSuiteValue;
+  if (randomSuiteIndex <= 1) {
+    topSuite.style.color = "red";
+    bottomSuite.style.color = "red";
+  }
+}
 window.onload = function() {
+  changeCard();
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  // const btn = document.createElement("button");
+  //btn.style.backgoundColor = "red";
+  //btn.style.color = "white";
+  //btn.style.fontSize = "50px";
+  //btn.innerText = "Draw";
+
+  //btn.addEventListener("click", fn);
+
+  //document.body.appendChild(btn);
 };
